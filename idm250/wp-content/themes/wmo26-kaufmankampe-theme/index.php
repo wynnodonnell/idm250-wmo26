5eda3f07fac7deecb80747deac95e790479d02b4
Wynn O'Donnell <wmo26@drexel.edu>
2020-07-14 12:37:04 -0700

    Core WP Files

---
+++ idm250/wp-content/themes/wmo26-kaufmankampe-theme/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>IDM 250</title><!--This is the text that will appear on the tab in the browser-->
+        <link rel="stylesheet" href="style.css"><!--This loads the regular style sheet for this file-->
+    </head>
+    <body>
+        idm250
+</body>
+</html>
