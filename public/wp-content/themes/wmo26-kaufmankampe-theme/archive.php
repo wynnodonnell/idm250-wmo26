fd881c6e69a46b8e2cdd076c00821b2c7365b17d
wynnodonnell <55889686+wynnodonnell@users.noreply.github.com>
2020-09-04 18:54:25 -0400

    Updated Final

--- public/wp-content/themes/wmo26-kaufmankampe-theme/archive.php
+++ public/wp-content/themes/wmo26-kaufmankampe-theme/archive.php
@@ -5,32 +5,24 @@
  * Learn more: http://codex.wordpress.org/Template_Hierarchy
  *
  */
-?>
-<?php get_header();?>
-<main class="main-content">
-    <div class="container">
-        <h1 class="archive__title"><?php echo single_term_title(); ?></h1>
+<?php 
+// the query
+$wpb_all_query = new WP_Query(array('post_type'=>'post', 'post_status'=>'publish', 'posts_per_page'=>-1)); ?>
 
-        <div class="archive__desc">
-            <?php echo term_description();?>
-        </div>
+<?php if ( $wpb_all_query->have_posts() ) : ?>
 
-        <div class="archive__posts split-3">
-            <?php if (have_posts()) : ?>
-            <?php while (have_posts()) : the_post(); ?>
-            <article class="article-block column">
-                <a href="<?php the_permalink(); ?>">
-                    <?php the_post_thumbnail(); ?>
-                    <h2><?php the_title(); ?></h2>
-                </a>
-            </article>
-            <?php endwhile; ?>
-            <?php else: ?>
-            <h2>Sorry, no post found</h2>
-            <?php endif; ?>
-        </div>
+<ul>
 
-    </div>
-</main>
+    <!-- the loop -->
+    <?php while ( $wpb_all_query->have_posts() ) : $wpb_all_query->the_post(); ?>
+    <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+    <?php endwhile; ?>
+    <!-- end of the loop -->
 
-<?php get_footer(); ?>
+</ul>
+
+<?php wp_reset_postdata(); ?>
+
+<?php else : ?>
+<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+<?php endif; ?>
