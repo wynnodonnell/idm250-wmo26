227a5d3207cd24467ff56b28946b17a92b520bc2
wynnodonnell <55889686+wynnodonnell@users.noreply.github.com>
2020-09-04 17:13:48 -0400

    added back

---
+++ public/wp-content/themes/wmo26-kaufmankampe-theme/footer.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ */
+?>
+<footer class="footer">
+    <div class="foot">
+        7525 SE 24TH STREET, SUITE 670 :: MERCER ISLAND, WASHINGTON 98040 :: P / 206 275 2277 F / 206 275 2278 :: <a class="email" href="mailto:INFO@KAUFMANKAMPE.COM">INFO@KAUFMANKAMPE.COM</a> © 2020 KAUFMAN KAMPE ADVISORS LLC
+    </div>
+</footer>
+<?php wp_footer(); ?>
+</body>
+
+</html>
